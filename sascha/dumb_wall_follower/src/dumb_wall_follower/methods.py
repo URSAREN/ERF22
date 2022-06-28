cf2b8db9ba57c229ad87560dda8a757d4691f0b8
kutovyi <kutovyi@igmr.rwth-aachen.de>
2022-06-28 20:10:42 +0200

    Updated structure. Minimal distance and heading done. Move base should be working.

--- sascha/dumb_wall_follower/src/dumb_wall_follower/methods.py
+++ sascha/dumb_wall_follower/src/dumb_wall_follower/methods.py
@@ -3,6 +3,7 @@ import math
 import rospy
 from std_msgs.msg import Bool
 
+
 def pol2cart(d, angle):
     """
     Transforms polar point coordinates to cartesian according to right-hand rule.
@@ -20,7 +21,6 @@ def pol2cart(d, angle):
     y = d * math.sin(angle)
     return [x,y]
     
-
 
 def distance_between_points(point1, point2):
         """
@@ -89,54 +89,6 @@ def project_point_on_line(point, line):
             return [[x_inter, y_inter], distance]
 
 
-def range_by_angle_from_laser_scan(laser_scan_msg, angle):
-    """
-    Returns nearest measered range to specified angle.
-
-    :param laser_scan_msg: A LaserScan msg from lidar
-    :type laser_scan_msg: nav_msgs.LaserScan
-    :param angle: Required angle in degrees (-180, 180)
-    :type angle: float or int
-
-    :return: Range [m] to the obstacle in the direction specified. None if out of range or scan angle boundaries
-    :rtype: float or None
-    """
-
-    # Unpack values for convenience
-    angle_min = 0.0
-    angle_max = float(laser_scan_msg.angle_max) - float(laser_scan_msg.angle_min)  # 360
-    range_min = float(laser_scan_msg.range_min)
-    range_max = float(laser_scan_msg.range_max)
-    angle_increment = float(laser_scan_msg.angle_increment)
-    distances = np.asarray(laser_scan_msg.ranges)
-
-    # angle = -angle # due to laser scan being in opposite direction.
-
-    if angle < -180 or angle > 180:
-        print('Provided angle is out of bounds of [-180, 180]')
-        return None
-    angle = math.radians(angle+180)
-
-
-
-    # Angle probably is in between two points, so will return average of 2 nearest measurements
-    angle_id_floor = math.floor((angle - angle_min) / angle_increment)
-    angle_id_ceil = math.ceil((angle - angle_min) / angle_increment)
-
-    # Safety precaution
-    if angle_id_ceil > len(distances):
-        angle_id_ceil = len(distances)
-
-    dist_floor = distances[angle_id_floor]
-
-    dist_ceil = distances[angle_id_ceil]
-    dist_avg = (dist_floor + dist_ceil) / 2
-    if dist_avg == math.inf:
-        print('Distance is at inf range.')
-        return None
-
-    return dist_avg
-
 def check_threshold(value_is, value_should, thresh):
     """
     Checks if value_is is within a threshold of value_should.
@@ -156,116 +108,6 @@ def check_threshold(value_is, value_should, thresh):
         return True
     else:
         return False
-
-def determine_turn_direction(laser_scan_msg, robot_angle_to_wall, wall_distance_required, distance_threshold=0.1, angles=[90, 45, 0]):
-    # TODO: rewrite for new cases (when robot will ride back -> the directions will be switched.) i.e. test if it is already functions.
-    """
-    Determines what type of turn to make. Suitable for 3 measurements.
-
-    :param laser_scan_msg: A LaserScan msg from lidar
-    :type laser_scan_msg: nav_msgs.LaserScan
-    :param robot_angle_to_wall: relative robot's angle [deg] to the wall. >0 if oriented to a wall, <0 if opposite.
-    :type robot_angle_to_wall: float 
-    :param wall_distance_required: should-be-distance at first angle (i.e. distance to the wall at 90 deg).
-    :type wall_distance_required: float
-    :param distance_threshold: determines at what point distance_is equals to  distance_should (0.1 = +-10% of distance_should).
-    :type distance_threshold: float
-    :param angles: Three angles at which distances are measured (i.e. location of sensors).
-    :type angles: list
-
-    :return: Assumed direction of the next turn. 
-        "right", "coming_right"
-        "left", "coming_left"
-        "error" if false range readings
-        None if no turn required.
-
-    :rtype: str or None
-    """
-
-    robot_angle_to_wall = math.radians(robot_angle_to_wall)
-
-    # d3
-    d3_is = range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[0])
-    if d3_is is None:
-        return "error"
-    d3_should = wall_distance_required /  math.cos(robot_angle_to_wall)
-
-    # d2 
-    ang_d3_d2 = angles[1] - angles[0]
-    ang_d2 = math.radians(robot_angle_to_wall + ang_d3_d2)
-    d2_is = range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[1])
-    if d2_is is None:
-        return "error"
-    d2_should = wall_distance_required / math.cos(ang_d2)
-
-    #d1
-    d1_is = range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[2])
-    if d1_is is None:
-        return "error"
-    print ("################################")
-    print(f'Measured d3: {d3_is}')
-    print(f'Calculated d3: {d3_should}')
-    print(f'Measured d2: {d2_is}')
-    print(f'Calculated d2: {d2_should}')
-    print(f'Measured d1: {d1_is}')
-
-    print(f'd3 thresh: {check_threshold(d3_is, d3_should, distance_threshold)}')
-    print(f'd2 thresh: {check_threshold(d2_is, d2_should, distance_threshold)}')
-    print(f'd1 thresh: {check_threshold(d1_is, wall_distance_required, distance_threshold)}')
-
-    # Cases:
-    if check_threshold(d3_is, d3_should, distance_threshold) and check_threshold(d2_is, d2_should, distance_threshold) \
-        and d1_is > wall_distance_required * (1.0 + distance_threshold):
-        return None
-
-    elif check_threshold(d3_is, d3_should, distance_threshold) and check_threshold(d2_is, d2_should, distance_threshold) \
-        and check_threshold(d1_is, wall_distance_required, distance_threshold):
-        return "coming_right"
-
-    elif check_threshold(d3_is, d3_should, distance_threshold) \
-        and d1_is < wall_distance_required * (1.0 - distance_threshold):
-        return "right"
-
-    elif check_threshold(d3_is, d3_should, distance_threshold) and not check_threshold(d2_is, d2_should, distance_threshold) \
-        and d1_is > wall_distance_required * (1.0 + distance_threshold):
-        return "coming_left"  
-
-    elif not check_threshold(d3_is, d3_should, distance_threshold) and not check_threshold(d2_is, d2_should, distance_threshold) \
-        and d1_is > wall_distance_required * (1.0 + distance_threshold):
-        return "left"
-    
-    else:
-        print('Case not covered.')
-        return "error"
-
-
-def get_shortest_distance(laser_scan_msg, left_border=100, right_border=-100):
-    """
-    Determines the shortest distance in the valid area in front of the robot
-
-    :param laser_scan_msg: A LaserScan msg from lidar
-    :type laser_scan_msg: nav_msgs.LaserScan
-
-    return: distance and angle in radians
-
-    :rtype: list
-    """
-    angle_min = 0.0
-    angle_max = float(laser_scan_msg.angle_max) - float(laser_scan_msg.angle_min)  # 360
-    range_min = float(laser_scan_msg.range_min)
-    range_max = float(laser_scan_msg.range_max)
-    angle_increment = float(laser_scan_msg.angle_increment)
-    distances = np.asarray(laser_scan_msg.ranges)
-
-    # Angle probably is in between two points, so will return average of 2 nearest measurements
-    angle_id_left = int((left_border - angle_min) / angle_increment)
-    angle_id_right = int((right_border - angle_min) / angle_increment)
-
-    min_dist = min(distances[angle_id_left:angle_id_right])
-    min_angle= np.argmin(distances[angle_id_left:angle_id_right]) * angle_increment
-    min_angle = -(min_angle - 180)
-
-    return [min_dist, min_angle]
 
 
 def tunnel_blocker(state: bool):
--- sascha/dumb_wall_follower/src/main_node.py
+++ sascha/dumb_wall_follower/src/main_node.py
@@ -1,4 +1,3 @@
-from re import I
 import rospy 
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
@@ -10,54 +9,82 @@ from geometry_msgs.msg import Point, Quaternion, Pose
 from tf2_geometry_msgs import PoseStamped
 import tf2_ros
 
-class Robot():
-    def __init__(self, name, corner_map):
-        self.name = name
+class Robot:
+    def __init__(self):
+        rospy.init_node('main_node', anonymous=True)
+        self.name = rospy.get_param('~name')
         self.corner_map = np.asarray(rospy.get_param('~corner_map'))
         self.last_corner_id = None
         self.corner_automaton_state = None
 
-        self.max_linear_speed = rospy.get_param('~max_linear_speed') # m/s
-        self.max_angular_speed = rospy.get_param('~max_angular_speed')  # rad/s
-        self.distance_to_wall = rospy.get_param('~distance_to_wall') # m
-        self.sensor_position = 0  # distance from sensor's center to robot's center in [m]
+        self.max_linear_speed = rospy.get_param('~max_linear_speed')
+        self.max_angular_speed = rospy.get_param('~max_angular_speed')
+        self.distance_to_wall = rospy.get_param('~room_distance_to_wall')
 
-        self.angles_left_wall_follow = [90, 60, 0] # d3, d2, d1 in deg
-        self.angles_right_wall_follow = [-90, -50, 0] # d3, d2, d1 in deg
+        self.angles_left_wall_follow = rospy.get_param('~angles_left_wall_follow')
+        self.angles_right_wall_follow = rospy.get_param('~angles_right_wall_follow')
         self.current_wall_location = 'left'
 
-        rospy.init_node(self.name, anonymous=True)
         self.tf_listener = tf.TransformListener()
         
-        self.goal_pose_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped)
-
-    def init_listener(self):
-        """
         
-        """
-        rospy.Subscriber("/scan", LaserScan, self.main_structure)
+        rospy.Subscriber(rospy.get_param('~laser_scan_topic'), LaserScan, self.test) # TODO: connect to main_structure
+        self.robot_mover = self.RobotMover()
         rospy.spin()
+        
 
-    def get_to_initial_pose(self, laser_scan_msg):
-        # dist, angle = get_shortest_distance(laser_scan_msg)
-        dist = 0
-        angle = math.radians(90)
-        x_shortest, y_shortest = pol2cart(dist, angle)
+    def range_by_angle_from_laser_scan(self, laser_scan_msg, angle):
+        """
+        Returns nearest measered range to specified angle.
 
-        my_pose = Pose()
+        :param laser_scan_msg: A LaserScan msg from lidar
+        :type laser_scan_msg: nav_msgs.LaserScan
+        :param angle: Required angle in degrees (-180, 180)
+        :type angle: float or int
+
+        :return: Range [m] to the obstacle in the direction specified. None if out of range or scan angle boundaries
+        :rtype: float or None
+        """
+
+        # Unpack values for convenience
+        angle_min = 0.0
+        angle_max = float(laser_scan_msg.angle_max) - float(laser_scan_msg.angle_min)  # 360
+        range_min = float(laser_scan_msg.range_min)
+        range_max = float(laser_scan_msg.range_max)
+        angle_increment = float(laser_scan_msg.angle_increment)
+        distances = np.asarray(laser_scan_msg.ranges)
+
+        if angle < -180 or angle > 180:
+            print('Provided angle is out of bounds of [-180, 180]')
+            return None
+        angle = math.radians(angle+180)
+
+        # Angle probably is in between two points, so will return average of 2 nearest measurements
+        angle_id_floor = math.floor((angle - angle_min) / angle_increment)
+        angle_id_ceil = math.ceil((angle - angle_min) / angle_increment)
+
+        # Safety precaution
+        if angle_id_ceil > len(distances):
+            angle_id_ceil = len(distances)
+
+        dist_floor = distances[angle_id_floor]
+        dist_ceil = distances[angle_id_ceil]
+        dist_avg = (dist_floor + dist_ceil) / 2
+        
+        if dist_avg == math.inf:
+            print('Distance is at inf range.')
+            return None
+
+        return dist_avg
 
 
-        my_pose.position.x = x_shortest
-        my_pose.position.y = y_shortest
-        my_pose.position.z = 0
+    def test(self, laser_scan_msg):
+        range, heading = self.get_shortest_distance(laser_scan_msg, left_border=100, right_border=-100)
+        print(f'Heading: {math.degrees(heading)}')
+        print(f'Distance: {range}')
+        # test_pose = self.error2pose(0, 0)
+        # self.robot_mover.send_pose_to_move_base(test_pose)
 
-        q = quaternion_from_euler(0, 0, angle)
-        my_pose.orientation.x = q[0]
-        my_pose.orientation.y = q[1]
-        my_pose.orientation.z = q[2]
-        my_pose.orientation.w = q[3]
-
-        self.goal2move_base(my_pose)
 
 
 
@@ -66,21 +93,21 @@ class Robot():
         Main logic of the robot.
         """
         
-        dist, angle = get_shortest_distance(laser_scan_msg)
+        dist, angle = self.get_shortest_distance(laser_scan_msg)
         # orientate to angle
         scan_gap = 30
-        dl = range_by_angle_from_laser_scan(laser_scan_msg, scan_gap)
-        dr = range_by_angle_from_laser_scan(laser_scan_msg, -scan_gap)
+        dl = self.range_by_angle_from_laser_scan(laser_scan_msg, scan_gap)
+        dr = self.range_by_angle_from_laser_scan(laser_scan_msg, -scan_gap)
         if (abs(dl-dr) > 0.2 * dl):
             # turn 180 degree
-            dl = range_by_angle_from_laser_scan(laser_scan_msg, scan_gap)
-            dr = range_by_angle_from_laser_scan(laser_scan_msg, -scan_gap)
+            dl = self.range_by_angle_from_laser_scan(laser_scan_msg, scan_gap)
+            dr = self.range_by_angle_from_laser_scan(laser_scan_msg, -scan_gap)
         # drive to wall (+ 60 cm distance)
         # rotate 90 degree right
 
         left_turns = 0
         while True:
-            turn_direction = determine_turn_direction()
+            turn_direction = self.determine_turn_direction()
             if turn_direction == 'left':
                 if left_turns == 0:
                     self.last_corner_id = 3
@@ -143,6 +170,7 @@ class Robot():
             8.4 Send point from 6.3 to move_base
         """        
 
+
     def measure_distances_to_wall(self, laser_scan_msg):
         """
         Returns measured distances to the wall.
@@ -155,35 +183,17 @@ class Robot():
         """
 
         if self.current_wall_location == 'left':
-            d3 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[0])
-            d2 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[1])
-            d1 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[2])
+            d3 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[0])
+            d2 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[1])
+            d1 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_left_wall_follow[2])
         elif self.current_wall_location == 'right':
-            d3 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[0])
-            d2 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[1])
-            d1 = range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[2])
+            d3 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[0])
+            d2 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[1])
+            d1 = self.range_by_angle_from_laser_scan(laser_scan_msg, self.angles_right_wall_follow[2])
         else:
             raise NotImplementedError('False wall location was given. Only "right" or "left".')
 
         return [d3, d2, d1]
-
-
-    
-    # def adjust_parallel_to_wall(self, laser_scan_msg):
-    #     """
-    #     Adjust the robot velocities to reduce difference between d3 and d2.
-
-    #     :param laser_scan_msg: A LaserScan msg from lidar
-    #     :type laser_scan_msg: nav_msgs.LaserScan
-
-    #     """
-        
-    #     d3_measured, d2_measured, d1_measured = self.measure_distances_to_wall(laser_scan_msg)
-    #     relative_angle = self.calculate_relative_angle_to_wall(d3_measured, d2_measured)
-    #     linear_error, angular_error = self.calculate_position_error(d3_measured, d2_measured)
-
-
-
 
 
     def calculate_position_error_laser_frame(self, d3_measured, d2_measured):
@@ -205,13 +215,13 @@ class Robot():
             point_d3 = pol2cart(d3_measured, math.radians(self.angles_left_wall_follow[0]))
             point_d2 = pol2cart(d2_measured, math.radians(self.angles_left_wall_follow[1]))
             d2d3 = [point_d3, point_d2] 
-            _, real_distance = project_point_on_line([-self.sensor_position, 0], d2d3)
+            _, real_distance = project_point_on_line([0, 0], d2d3)
             linear_error = real_distance - self.distance_to_wall
         elif self.current_wall_location == 'right':
             point_d3 = pol2cart(d3_measured, math.radians(self.angles_right_wall_follow[0]))
             point_d2 = pol2cart(d2_measured, math.radians(self.angles_right_wall_follow[1]))
             d2d3 = [point_d3, point_d2] 
-            _, real_distance = project_point_on_line([-self.sensor_position, 0], d2d3)
+            _, real_distance = project_point_on_line([0, 0], d2d3)
             linear_error = self.distance_to_wall - real_distance
 
 
@@ -268,78 +278,186 @@ class Robot():
         else:
             raise NotImplementedError('False wall location was given. Only "right" or "left".')
 
-    def laser2base_error(self, linear_error, angular_error):
+
+    def error2pose(self, linear_error, angular_error):
         """
-        Converts linear and angular errors from laser to base_link frame.
+        Transforms errors into a pose in laser coord. frame.
 
         :param linear_error: Linear distance error from laser frame origin to wall (perpendicular) in [m].
         :type linear_error: float
         :param angular_error: Angular error from laser frame origin to wall in [rad].
         :type angular_error: float
 
-        :return: Recalculated errors in base_link frame [base_lin_error, base_ang_error].
-        :rtype: list[float, float]
+        :return: point in cartesian space.
+        :rtype: geometry_msgs/Pose
+        
         """
+        
         my_pose = Pose()
-
-
 
         my_pose.position.y = linear_error * math.cos(-angular_error)
         my_pose.position.x = linear_error * math.sin(-angular_error)
         my_pose.position.z = 0
+
         q = quaternion_from_euler(0, 0, angular_error)
         my_pose.orientation.x = q[0]
         my_pose.orientation.y = q[1]
         my_pose.orientation.z = q[2]
         my_pose.orientation.w = q[3]
 
-        transformed_pose = self.transform_pose(my_pose, 'laser', 'base_link')
-        return transformed_pose
-    
-    @staticmethod
-    def transform_pose(input_pose, from_frame, to_frame):
+        return my_pose
 
-        # **Assuming /tf2 topic is being broadcasted
-        tf_buffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tf_buffer)
 
-        pose_stamped = PoseStamped()
-        pose_stamped.pose = input_pose
-        pose_stamped.header.frame_id = from_frame
-        pose_stamped.header.stamp = rospy.Time.now()
-
-        try:
-            # ** It is important to wait for the listener to start listening. Hence the rospy.Duration(1)
-            output_pose_stamped = tf_buffer.transform(pose_stamped, to_frame, rospy.Duration(1))
-            return output_pose_stamped.pose
-
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            raise
-
-    def goal2move_base(self, goal_pose_in_robot_frame):
+    def determine_turn_direction(self, laser_scan_msg, robot_angle_to_wall, wall_distance_required, distance_threshold=0.1, angles=[90, 45, 0]):
+        # TODO: rewrite for new cases (when robot will ride back -> the directions will be switched.) i.e. test if it is already functions.
         """
-        Send dumb command to move_base to test if it is working.
+        Determines what type of turn to make. Suitable for 3 measurements.
 
-        :param goal_pose_in_robot_frame: Pose for the robot to move to.
-        :type goal_pose_in_robot_frame: geometry_msgs/Pose
+        :param laser_scan_msg: A LaserScan msg from lidar
+        :type laser_scan_msg: nav_msgs.LaserScan
+        :param robot_angle_to_wall: relative robot's angle [deg] to the wall. >0 if oriented to a wall, <0 if opposite.
+        :type robot_angle_to_wall: float 
+        :param wall_distance_required: should-be-distance at first angle (i.e. distance to the wall at 90 deg).
+        :type wall_distance_required: float
+        :param distance_threshold: determines at what point distance_is equals to  distance_should (0.1 = +-10% of distance_should).
+        :type distance_threshold: float
+        :param angles: Three angles at which distances are measured (i.e. location of sensors).
+        :type angles: list
 
+        :return: Assumed direction of the next turn. 
+            "right", "coming_right"
+            "left", "coming_left"
+            "error" if false range readings
+            None if no turn required.
 
+        :rtype: str or None
         """
 
+        robot_angle_to_wall = math.radians(robot_angle_to_wall)
 
-        pose_stamped = PoseStamped()
-        pose_stamped.header.frame_id = 'base_link'
-        pose_stamped.pose = goal_pose_in_robot_frame
-        print(f'Sending following to move_base: {pose_stamped}')
+        # d3 related
+        d3_is = self.range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[0])
+        if d3_is is None:
+            return "error"
+        d3_should = wall_distance_required /  math.cos(robot_angle_to_wall)
+        is_d3_thresh = check_threshold(d3_is, d3_should, distance_threshold)
 
-        # rospy.init_node('dumb_command_publisher', anonymous=True)
-        # goal_pose_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped)
-        my_twist = Twist()
-        my_twist.angular.z = 0.5
-        self.goal_pose_publisher.publish(pose_stamped)
-    
+        # d2 related
+        ang_d3_d2 = angles[1] - angles[0]
+        ang_d2 = math.radians(robot_angle_to_wall + ang_d3_d2)
+        d2_is = self.range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[1])
+        if d2_is is None:
+            return "error"
+        d2_should = wall_distance_required / math.cos(ang_d2)
+        is_d2_thresh = check_threshold(d2_is, d2_should, distance_threshold)
+
+        #d1 related
+        d1_is = self.range_by_angle_from_laser_scan(laser_scan_msg=laser_scan_msg, angle=angles[2])
+
+        if d1_is is None:
+            return "error"
+
+        # Cases:
+        if is_d3_thresh and is_d2_thresh and d1_is > wall_distance_required * (1.0 + distance_threshold):
+            return None
+
+        elif is_d3_thresh and is_d2_thresh and check_threshold(d1_is, wall_distance_required, distance_threshold):
+            return "coming_right"
+
+        elif is_d3_thresh and d1_is < wall_distance_required * (1.0 - distance_threshold):
+            return "right"
+
+        elif is_d3_thresh and not is_d2_thresh and d1_is > wall_distance_required * (1.0 + distance_threshold):
+            return "coming_left"  
+
+        elif not is_d3_thresh and not is_d2_thresh and d1_is > wall_distance_required * (1.0 + distance_threshold):
+            return "left"
         
+        else:
+            print('Case not covered.')
+            return "error"
+
+
+    def get_shortest_distance(self, laser_scan_msg, left_border=90, right_border=-90):
+        """
+        Determines the shortest distance in the valid area in front of the robot
+
+        :param laser_scan_msg: A LaserScan msg from lidar
+        :type laser_scan_msg: nav_msgs.LaserScan
+        :param left_border: left fov angle in [deg]
+        :type left_border: int
+        :param right_border: right fov angle in [deg]
+        :type right_border: int
+
+        return: distance and angle in [rad]
+
+        :rtype: list
+        """
+
+        angle_increment = float(laser_scan_msg.angle_increment)
+        distances = np.asarray(laser_scan_msg.ranges)
+
+        angle_diff_left = math.radians(180 - left_border)
+        angle_diff_right = math.radians(180 - right_border)
+
+        angle_id_left = int(angle_diff_left / angle_increment)
+        angle_id_right = int(angle_diff_right / angle_increment)
+
+        min_range = min(distances[angle_id_left:angle_id_right])
+        min_id = np.argmin(distances[angle_id_left:angle_id_right]) + angle_id_left
+        min_angle = min_id * angle_increment - math.radians(180)
+
+        return [min_range, min_angle]
+
+
+    class RobotMover:
+        def __init__(self):
+            self.goal_pose_publisher = rospy.Publisher(rospy.get_param('~publish_pose_to_topic'), PoseStamped, queue_size=10)
+
+        
+        def send_pose_to_move_base(self, goal_pose, goal_pose_frame='laser'):
+            """
+            Sends pose to move_base to follow.
+
+            :param goal_pose: Pose for the robot to move to expressed in its own frame.
+            :type goal_pose: geometry_msgs/Pose
+            :param goal_pose_frame: Frame in which goal_pose is expressed.
+            :type goal_pose_frame: str
+
+            """
+
+            my_pose_in_base = self.transform_pose(input_pose=goal_pose, from_frame=goal_pose_frame, to_frame='map')
+
+            pose_stamped = PoseStamped()
+
+            pose_stamped.header.frame_id = 'base_link'
+            pose_stamped.pose = my_pose_in_base
+
+            self.goal_pose_publisher.publish(pose_stamped)
+
+
+        @staticmethod
+        def transform_pose(input_pose, from_frame, to_frame):
+
+            # **Assuming /tf2 topic is being broadcasted
+            tf_buffer = tf2_ros.Buffer()
+            listener = tf2_ros.TransformListener(tf_buffer)
+
+            pose_stamped = PoseStamped()
+            pose_stamped.pose = input_pose
+            pose_stamped.header.frame_id = from_frame
+            pose_stamped.header.stamp = rospy.Time.now()
+
+            try:
+                # ** It is important to wait for the listener to start listening. Hence the rospy.Duration(1)
+                output_pose_stamped = tf_buffer.transform(pose_stamped, to_frame, rospy.Duration(1))
+                return output_pose_stamped.pose
+
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                pass
+
+            
+
 if __name__ == '__main__':
-    corner_map_a = rospy.get_param('/corner_map')
     robotA = Robot()
-    robotA.initiate_listener()
+    
